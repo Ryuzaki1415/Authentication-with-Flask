b8e448f565f0fd5c2b11f610bbf84c5ff1f5b794
dheeraj <dheerajnunni@gmail.com>
2024-01-21 22:42:37 +0530

    implemented register and login, created dashboard

--- app.py
+++ app.py
@@ -15,21 +15,79 @@ app.config['SECRET_KEY'] = 'thisisasecretkey'
 db.init_app(app)
 
 
+
+
+login_manager = LoginManager()
+login_manager.init_app(app)
+login_manager.login_view = 'login'
+
+@login_manager.user_loader
+def load_user(user_id):
+    return User.query.get(int(user_id))
+
+
+
 with app.app_context():
     class User(db.Model, UserMixin):
         id = db.Column(db.Integer, primary_key=True)
         username = db.Column(db.String(20), nullable=False, unique=True)
         password = db.Column(db.String(80), nullable=False)
+    
+    
+    class RegisterForm(FlaskForm):
+        username = StringField(validators=[
+                            InputRequired(), Length(min=4, max=20)], render_kw={"placeholder": "Username"})
+
+        password = PasswordField(validators=[
+                                InputRequired(), Length(min=8, max=20)], render_kw={"placeholder": "Password"})
+
+        submit = SubmitField('Register')
+    
+    
+    def validate_username(self, username):  # to check if it is a unique user name
+        existing_user_username = User.query.filter_by(  #queriying the user table
+            username=username.data).first()
+        if existing_user_username:
+            raise ValidationError(
+                'That username already exists. Please choose a different one.')
+            
+            
+    class LoginForm(FlaskForm):
+        username = StringField(validators=[
+                            InputRequired(), Length(min=4, max=20)], render_kw={"placeholder": "Username"})
+
+        password = PasswordField(validators=[
+                                InputRequired(), Length(min=8, max=20)], render_kw={"placeholder": "Password"})
+
+        submit = SubmitField('Login')
 
     @app.route("/")
     def home():
         return render_template('home.html')
-    @app.route("/login")
+    @app.route("/login",methods=['GET','POST'])
     def login():
-        return render_template("login.html")
-    @app.route("/register")
+        form=LoginForm()
+        if form.validate_on_submit():
+            user = User.query.filter_by(username=form.username.data).first()
+            if user:
+                if bcrypt.check_password_hash(user.password, form.password.data):
+                    login_user(user)
+                    return redirect(url_for('dashboard'))
+        return render_template("login.html",form=form)  #passing form to html
+    @app.route("/register",methods=['GET','POST'])
     def register():
-        return render_template("register.html")
+        form=RegisterForm()
+        if form.validate_on_submit():
+            hashed_password = bcrypt.generate_password_hash(form.password.data)
+            new_user = User(username=form.username.data, password=hashed_password)
+            db.session.add(new_user)
+            db.session.commit()
+            return redirect(url_for('login'))
+        return render_template("register.html",form=form)
+    @app.route('/dashboard', methods=['GET', 'POST'])
+    @login_required
+    def dashboard():
+        return render_template('dashboard.html')
 
 
     if __name__ == "__main__":
